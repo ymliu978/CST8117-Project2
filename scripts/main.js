5e661a4459a17269e1240cb6b66afc9a65c1b415
Sebastian <43918054+SebastianKamal@users.noreply.github.com>
2021-11-24 20:54:30 -0500

    Added dark mode. It works for the main page so far as it changes the body as well as the headings and their corresponding text.

--- scripts/main.js
+++ scripts/main.js
@@ -54,3 +54,81 @@ window.addEventListener("resize", function () {
         responsiveMenuOpen.style.display = "block";
     }
 });
+
+/*******************************************************************************
+              Global user experience functionality. (ie. dark mode)
+ *******************************************************************************/
+
+// Getting all the user option buttons.
+var uxButtons = document.querySelectorAll("div[class='features']");
+
+// Getting both the non responsive and responsive theme toggle buttons.
+let themeToggleButtons = [uxButtons[0].children[0], uxButtons[1].children[0]];
+
+// Getting both the non responsive and responsive language buttons.
+let languageToggleButtons = [uxButtons[0].children[1], uxButtons[1].children[1]]; // Toggle for language.
+
+/*****************************
+    Dark mode functionality.
+ *****************************/
+
+// Adding the CSS for the themes.
+let themeStyles = document.createElement("style");
+themeStyles.innerHTML = ".dark-mode-heading { border: 1px solid rgb(226, 226, 226); color: rgb(226, 226, 226); } .dark-mode-text { color: rgb(226, 226, 226); }";
+document.querySelector("head").appendChild(themeStyles);
+
+/*
+Checking if the theme was last set to dark. If it was, it will update the theme to be dark. If it was light,
+it doesn't have to do anything because the default class styles are already dark.
+
+Also checks if the theme is undefined, because if it is, it means the theme is light, as it's the user's
+first time on the page.
+*/
+if (localStorage.theme == "dark") {
+    enableDarkTheme();
+} else if (localStorage.theme == undefined) {
+    localStorage.theme = "light";
+}
+
+// Event listener to make each of the theme toggle buttons work (both responsive and non responsive).
+themeToggleButtons.forEach((button) => {
+    button.addEventListener("click", function() {
+        // If the theme is light theme...
+        if (localStorage.theme == "light") {
+            // Set the theme to dark theme.
+            enableDarkTheme();
+        // If the theme is dark theme...
+        } else {
+            // Set the theme to light theme.
+            enableLightTheme();
+        }
+    });
+});
+
+// Enable light theme.
+function enableLightTheme() {
+    // Updating the theme reference to light.
+    localStorage.theme = "light";
+    // Setting the body background to white.
+    document.body.style.backgroundColor = "white";
+    // Setting the textinfo-headings to light mode.
+    document.querySelectorAll("div[class='textinfo-heading dark-mode-heading']").forEach((heading) => {
+        Array.from(heading.parentNode.children).forEach((child) => {
+            child.classList == "textinfo-heading dark-mode-heading" ? child.classList = "textinfo-heading" : child.removeAttribute("class");
+        });
+    });
+}
+
+// Enable dark theme.
+function enableDarkTheme() {
+    // Updating the theme reference to dark.
+    localStorage.theme = "dark";
+    // Setting the body background to black.
+    document.body.style.backgroundColor = "black";
+    // Setting the textinfo-headings to dark mode.
+    document.querySelectorAll("div[class='textinfo-heading']").forEach((heading) => {
+        Array.from(heading.parentNode.children).forEach((child) => {
+            child.classList == "textinfo-heading" ? child.classList = "textinfo-heading dark-mode-heading" : child.classList = "dark-mode-text";
+        });
+    });
+}
